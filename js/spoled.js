9fcb7aa8d57beed53d244a2379ebb5b2121c11c8
KasiaWerner <kwerner@centrumcyfrowe.pl>
2018-05-16 12:12:13 +0200

    Update spoled.js

--- js/spoled.js
+++ js/spoled.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    var x = document.getElementById("myDIV");
+    var x = document.getElementById("sidebar_list");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
