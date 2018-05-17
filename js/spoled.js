43e1743eb2df22b3d98c6dbf28e6ceca5bd5cf23
KasiaWerner <kwerner@centrumcyfrowe.pl>
2018-05-17 15:09:03 +0200

    Update spoled.js

--- js/spoled.js
+++ js/spoled.js
@@ -1,3 +1,7 @@
+$('.sidebar1 li').attr('id', function(i) {
+   return 'page'+(i+1);
+});
+
 $(document).ready(function(){
     $(".button-sidebar").click(function(){
         $(".sidebar1").toggle();
