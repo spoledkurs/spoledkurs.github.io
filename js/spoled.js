ea9b6487cd9b7d8aca1dd8aaa546248b1753c3d6
KasiaWerner <kwerner@centrumcyfrowe.pl>
2018-07-19 12:18:31 +0200

    Update spoled.js

--- js/spoled.js
+++ js/spoled.js
@@ -2,11 +2,11 @@ $('.sidebar1 li').attr('id', function(i) {
    return 'child'+(i+1);
 });
 
-/*$('.sidebar1').hide(); */
+$('.sidebar1').hide(); 
 
 
 $('.module-name').children().click(function(){
-    $(this).children('.sidebar1').slideToggle('slow');     
+    $(this).children('.sidebar1').toggle('slow');     
 }).children('.sidebar1').click(function (event) {
     event.stopPropagation();
 });
