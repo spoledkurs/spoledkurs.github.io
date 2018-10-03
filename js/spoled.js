80aacc55672674bebbd6f0a11bb81e6046aaed74
KasiaWerner <kwerner@centrumcyfrowe.pl>
2018-10-03 13:30:40 +0200

    Update spoled.js

--- js/spoled.js
+++ js/spoled.js
@@ -3,7 +3,7 @@ $('.sidebar1 li').attr('id', function(i) {
 });
 
 
-/* $('.sidebar1').hide(); */
+ $('.sidebar1').hide(); 
 
 $('.module-name').children().click(function(){
     $(this).children('.sidebar1').slideToggle('slow');     
@@ -11,30 +11,5 @@ $('.module-name').children().click(function(){
     event.stopPropagation(); 
 });
 
-
-/*$('.module-name > li > a').click(function(){
-    event.preventDefault();
-    $(this).siblings('.sidebar1').slideToggle('slow');
-}); */
-
-/* $(document).ready(function(){
-    $('.sidebar1').hide(); 
-   
-    $('.module-name').children().click(function(){
-        $(this).children('.sidebar1').toggle('slow');
-    });
-}); */
-
-
-/* $('.module-name').children().click(function(){
-    $(this).children('.sidebar1').toggle('slow');     
-}).children('.sidebar1').click(function (event) {
-    event.stopPropagation();
-}); */
-
-/*$('.module-name').children().click(function(){
-
-    $(this).children('.sidebar1').slideToggle('slow');     
-});*/ 
 
 
